73655dbcee15f0f13115a3cec960dc079bcdc3bf
barath kumar <office.barath@gmail.com>
2021-11-21 16:35:36 +0530

    continue

--- logic.py
+++ logic.py
@@ -1,8 +1,15 @@
-#Break
+# #Break
+
+# for i in range(10):
+#   if i == 7:
+#     print("Loop Reached the Limit")
+#     break
+#   print(i)
+# print("Outside The Loop")
 
 for i in range(10):
-  if i == 7:
-    print("Loop Reached the Limit")
-    break
+  if i%2==0:
+    continue
   print(i)
-print("Outside The Loop")
+  
+    
