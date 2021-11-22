e661d0571746875924919e93fbd37f10a4510b7f
barath kumar <office.barath@gmail.com>
2021-11-22 21:56:06 +0530

    .

--- logic.py
+++ logic.py
@@ -56,6 +56,23 @@
 #   else:
 #         print("It Is Not Prime Number")
 
+# n= int(input("Enter the Number  "))
+# count = 0
+# n1 =2
+# while True:
+#   is_prime = True
+#   for i in range(2,n1//2+1):
+#     if n1%i ==0:
+#       is_prime=False
+#       break
+#   if is_prime==True:
+#     print(n1)
+#     count= count+1
+#   if count == n:
+#     break
+#   n1 = n1+1
+
+
    
 
 
