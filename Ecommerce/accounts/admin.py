ca43503880c69009a653377874be17ce25b2bfc9
barath kumar <office.barath@gmail.com>
2021-12-01 12:53:39 +0530

    admin pannel

--- Ecommerce/accounts/admin.py
+++ Ecommerce/accounts/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Account
+from django.contrib.auth.admin import UserAdmin
 
+
+class AccountAdmin(UserAdmin):
+  list_display = ('email','first_name','last_name','username','last_login','date_joined','is_active')
+
+  filter_horizontal = ()
+  list_filter = ()
+  fieldsets= ()
 
 # Register your models here.
-admin.site.register(Account)
+admin.site.register(Account,AccountAdmin)
