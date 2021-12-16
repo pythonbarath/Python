5ccdc0ca0de4779ec7c87e21a37150b0b2b1996c
barath kumar <office.barath@gmail.com>
2021-12-16 19:42:11 +0530

    add to cart not showing

--- carts/views.py
+++ carts/views.py
@@ -1,8 +1,9 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.http.response import HttpResponse
 from django.shortcuts import render,redirect 
 from store.models import Product
 from carts.models import Cart, CartItems 
-from  django.http import HttpResponse
+
 
 
 
@@ -40,13 +41,24 @@ def add_cart(request,product_id):
             quantity = 1,
             cart = cart,
         )
-        cart_item.save()
-    return HttpResponse(cart_item.product)
-    exit()   
+        cart_item.save()      
     return redirect('cart')
 
 
 
 
-def cart(request):
+def cart(request,total=0,quantity=0,cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItems.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    except ObjectDoesNotExist:
+        pass 
+    context = {
+        'total' : total,
+        'quantity' : quantity,
+        'cart_items' : cart_items,
+    }
     return render(request,'store/cart.html')
