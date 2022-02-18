56af17ede01cc5f10ff7d56d3945d4cd4b7fb522
Barath Kumar <office.barath@gmail.com>
2022-02-18 16:28:30 +0530

    "Added Variations -working on ADD TO CART"

--- carts/admin.py
+++ carts/admin.py
@@ -1,8 +1,19 @@
+from dataclasses import asdict
+
+from msilib import datasizemask
+from this import d
+from urllib.request import AbstractDigestAuthHandler
 from django.contrib import admin
 from .models import *
 # Register your models here.
 
 
-admin.site.register(Cart)
-admin.site.register(CartItems)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('cart_id', 'date_added')
+
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'cart', 'quantity', 'is_active')
+
+admin.site.register(Cart, CartAdmin)
+admin.site.register(CartItems, CartItemAdmin)
 
